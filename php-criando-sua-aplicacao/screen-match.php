054fbc68ab38158f048be8824fe463939371018e
Eliel-DM <elieldiasmatos@gmail.com>
2026-08-05 19:34:02 -0300

    Utlizando a match expression

--- php-criando-sua-aplicacao/screen-match.php
+++ php-criando-sua-aplicacao/screen-match.php
@@ -2,7 +2,7 @@
 
 echo "Bem-vindo(a) ao screen match! \n";
 
-$nomeFilme = "Top gun - Maverick";
+$nomeFilme = "Top Gun - Maverick";
 $anoLancamento = $argv[1] ?? 2022; // Solicita o valor via CLI, que caso não seja identificado envia o valor 2022. Anotado no caderno [ARGUMENTCOUNT] & [NULL COALESCING OPERATOR]
 $notaFilme = 8.8;
 $inclusoNoPlano = true;
@@ -19,3 +19,20 @@ $inclusoNoPlano = $planoPrime || $anoLancamento < 2020;
 echo "Nome do filme: " . $nomeFilme . "\n";
 echo "Nota do filme: " . $notaFilme . "\n";
 echo "Ano de Lançamento: " . $anoLancamento . "\n";
+
+if ($anoLancamento > 2022) {
+    echo "Esse filme é um lançamento\n";
+} elseif ($anoLancamento > 2020 && $anoLancamento <= 2022) {
+    echo "Esse filme ainda é novo\n";
+} else {
+    echo "Esse filme não é um lançamento\n";
+}
+
+$genero = match ($nomeFilme) {
+    "Top Gun - Maverick" => "ação",
+    "Thor: Ragnarok" => "super-Herói",
+    "Se beber não case" => "comédia",
+    default => "Generô não identificado",
+};
+
+echo "O gênero do filme é: " . $genero;
