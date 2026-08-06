31fb94d6905b8b1d212d768a2cc9db89748dbc03
Eliel-DM <elieldiasmatos@gmail.com>
2026-08-06 18:10:25 -0300

    Feito o código relacionado ao case 1, para validar o saldo da conta.

--- php-criando-sua-aplicacao/controle-de-saldo-bancario.php
+++ php-criando-sua-aplicacao/controle-de-saldo-bancario.php
@@ -12,26 +12,21 @@ Saldo atual: R$ 1000
 
 Gostaríamos que você supervisionasse o controle do saldo de uma conta. Nesse sentido, é importante incluir informações como o titular da conta, embora por enquanto consideremos que apenas o titular e o saldo sejam suficientes. Além disso, algumas operações devem ser possíveis, como consultar o saldo atual, fazer saques, depósitos ou sair do programa.
 */
+$titular = "Eliel Matos";
+$saldo = 1000;
 
 
 while (true) {
-    echo "
-    **********************
-    Titular: 
-    Saldo atual: 
-    **********************
-    1. Consultar Saldo Atual
-    2. Sacar valor 
-    3. Depositar valor
-    4. Sair
-    ";
+    echo "************************\n Titular: " . $titular . "\n Saldo: " . $saldo . "\n************************";
+    echo "\n1. Consultar Saldo Atual\n2. Sacar valor\n3. Depositar valor\n4. Sair\n************************\n";
 
     $validador = fgets(STDIN);
 
     switch ($validador) {
 
         case $validador == 1:
-            echo "Número 1 Selecionado\n";
+            echo "\n SALDO ATUAL:" . $saldo . "\n";
+            sleep(5);
             break;
         case $validador == 2:
             echo "Número 2 Selecionado\n";
@@ -44,3 +39,4 @@ while (true) {
             exit(1);
     }
 }
+// Acredito que implemntar ess parte com orientação a objetos faça mais sentido na minha cabeça, mas, vou seguir com o que foi ensinado até agora.
